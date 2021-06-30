dd5345d4347410f12c10d78e4489f01d64f7e554
Pavel <sa1zen@mail.ru>
2021-06-30 18:01:30 +0400

    Discount

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalPrice = Convert.ToInt32(Console.ReadLine());
+            int result = Discount(totalPrice);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }    
+
+
+        static int Discount(int x)
+        {
+            if (x >= 10000)
+            {
+                return Convert.ToInt32(x - (x * 0.2));
+
+            }
+            else
+            {
+                return x;
+            }
+        }   
+       
+    }
+   
+}
